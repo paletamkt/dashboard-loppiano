703b81a996dfcf402452290524b11f17d42d3786
Paleta Mkt <luiz@paletamarketing.com.br>
2026-06-16 19:36:17 -0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,7 +2,7 @@ const SUPABASE_URL =
   'https://zuwnaejrihlhdmbwoirw.supabase.co'
 
 const SUPABASE_ANON_KEY =
-  'SUA_ANON_KEY_AQUI'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1d25hZWpyaWhsaGRtYndvaXJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1NDcxMzQsImV4cCI6MjA5MjEyMzEzNH0.Jr2YWcIxYr8naMdhwziEIWgGVWKTeLwqFnFglB8s5LA'
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
