766589d874ca85ec5f32e580eda2555c056084c0
Paleta Mkt <luiz@paletamarketing.com.br>
2026-06-16 12:00:07 -0400

    Update dashboard.js

--- functions/dashboard.js
+++ functions/dashboard.js
@@ -1,5 +1,8 @@
-export async function onRequest() {
+export async function onRequest(context) {
+
   return Response.json({
-    teste: "funcionando"
+    supabaseUrl: !!context.env.SUPABASE_URL,
+    serviceRole: !!context.env.SUPABASE_SERVICE_ROLE
   })
+
 }
