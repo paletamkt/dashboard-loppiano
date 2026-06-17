96b808b65f7a5c22eec343e54cf057bb93852b1e
Paleta Mkt <luiz@paletamarketing.com.br>
2026-06-16 20:26:30 -0400

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -44,28 +44,3 @@ function animarNumero(
   requestAnimationFrame(atualizar)
 
 }
-
-function animarCards() {
-
-  document
-    .querySelectorAll('.kpi-card')
-    .forEach((card, index) => {
-
-      card.style.opacity = '0'
-      card.style.transform =
-        'translateY(20px)'
-
-      setTimeout(() => {
-
-        card.style.transition =
-          'all .4s ease'
-
-        card.style.opacity = '1'
-        card.style.transform =
-          'translateY(0)'
-
-      }, index * 120)
-
-    })
-
-}
